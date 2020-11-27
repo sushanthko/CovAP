eaf4df7308743277e2c62a073519b7167efa8c53
sushantk-stud <55709595+sushantk-stud@users.noreply.github.com>
2020-11-27 08:26:59 +0100

    Update CovAP.py

--- CovAP.py
+++ CovAP.py
@@ -3,13 +3,13 @@
 
 # # COVID-19 Analysis Platform
 
-# In[25]:
+# In[1]:
 
 
 from jupyter_dash import JupyterDash
 
 
-# In[26]:
+# In[2]:
 
 
 import dash
@@ -21,25 +21,47 @@ import dash_table as dt
 from dash.dependencies import Input, Output
 
 
-# In[27]:
+# In[3]:
 
 
 debug = pd.read_json("properties.json", orient="index").debug.value
 
 
-# In[28]:
+# In[4]:
 
 
 df = pd.read_csv("owid-covid-data.csv")
 
 
-# In[29]:
+# In[5]:
 
 
 countries = df.location.unique()
 
 
-# In[30]:
+# In[6]:
+
+
+def getFormat(column):
+    if(column in numeric_columns):
+        if("per_million" in column):
+            return {'specifier': ',.2f'}
+        else:
+            return {'specifier': ',.0f'}
+    return None
+
+    
+def getType(column):
+    if(column in numeric_columns):
+        return "numeric"
+    return "text"
+
+
+def getTitleText(text):
+    return text.title().replace('_', ' ').replace("Gdp", "GDP")
+
+
+# In[7]:
 
 
 dpm_countries = 20
@@ -65,36 +87,250 @@ dfmain_no_world = dfmain[~dfmain.iso_code.isin(['OWID_WRL'])] # Remove world row
 dfm = dfmain_no_world[['iso_code', 'location', 'total_cases_per_million', 'total_deaths_per_million']]
 
 
-# In[31]:
+# In[8]:
 
 
 header = html.H1("CovAP dashboard")
 last_updated = html.H6("Last updated on: "+df.date.max(),className="text-right")
 
 
-# In[32]:
+# In[9]:
 
 
-def getFormat(column):
-    if(column in numeric_columns):
-        if("per_million" in column):
-            return {'specifier': ',.2f'}
-        else:
-            return {'specifier': ',.0f'}
-    return None
+# Alcohol sales data
+alcohol_sales = pd.read_csv("Statistics on alcohol sales, by type of beverage, contents and quarter.csv", delimiter=";")
+alcohol_sales = alcohol_sales.set_index("type of beverage")
+alcohol_sales = alcohol_sales.rename(columns={
+ "Sales of alcoholic beverages per capita 15 years and over, as sold (litres) 2019K1": "Q1 2019 Sales",
+ "Sales of alcoholic beverages per capita 15 years and over, as sold (litres) 2019K2": "Q2 2019 Sales",
+ "Sales of alcoholic beverages per capita 15 years and over, as sold (litres) 2019K3": "Q3 2019 Sales",
+ "Sales of alcoholic beverages per capita 15 years and over, as sold (litres) 2019K4": "Q4 2019 Sales",
+ "Sales of alcoholic beverages per capita 15 years and over, as sold (litres) 2020K1": "Q1 2020 Sales",
+ "Sales of alcoholic beverages per capita 15 years and over, as sold (litres) 2020K2": "Q2 2020 Sales",
+ "Sales of alcoholic beverages per capita 15 years (litres) and over, pure alcohol 2019K1": "Q1 2019 Sales pure alcohol",
+ "Sales of alcoholic beverages per capita 15 years (litres) and over, pure alcohol 2019K2": "Q2 2019 Sales pure alcohol",
+ "Sales of alcoholic beverages per capita 15 years (litres) and over, pure alcohol 2019K3": "Q3 2019 Sales pure alcohol",
+ "Sales of alcoholic beverages per capita 15 years (litres) and over, pure alcohol 2019K4": "Q4 2019 Sales pure alcohol",
+ "Sales of alcoholic beverages per capita 15 years (litres) and over, pure alcohol 2020K1": "Q1 2020 Sales pure alcohol",
+ "Sales of alcoholic beverages per capita 15 years (litres) and over, pure alcohol 2020K2": "Q2 2020 Sales pure alcohol",  
+})
 
+alcohol_sales_all = alcohol_sales[["Q1 2019 Sales", "Q2 2019 Sales", "Q3 2019 Sales", "Q4 2019 Sales",
+                                   "Q1 2020 Sales", "Q2 2020 Sales"]]
+alcohol_sales_all = alcohol_sales_all.T
+alcohol_sales_all.columns.name = None
+new_indices = [i.replace("Sales", "").strip() for i in alcohol_sales_all.index]
+alcohol_sales_all = alcohol_sales_all.rename(index=dict(zip(alcohol_sales_all.index,new_indices)))
+
+alcohol_sales_pure_alcohol = alcohol_sales[["Q1 2019 Sales pure alcohol", "Q2 2019 Sales pure alcohol",
+                                   "Q3 2019 Sales pure alcohol", "Q4 2019 Sales pure alcohol", 
+                                   "Q1 2020 Sales pure alcohol", "Q2 2020 Sales pure alcohol"]]
+alcohol_sales_pure_alcohol = alcohol_sales_pure_alcohol.T
+alcohol_sales_pure_alcohol.columns.name = None
+# new_indices = [i.replace("Sales pure alcohol", "").strip() for i in alcohol_sales_pure_alcohol.index]
+alcohol_sales_pure_alcohol = alcohol_sales_pure_alcohol.rename(
+    index=dict(zip(alcohol_sales_pure_alcohol.index,new_indices)))
+alcohol_sales_all
+
+
+# In[10]:
+
+
+# Trips data
+trips = pd.read_csv("Trips (million), by type of destination, type of trip, contents and quarter.csv", delimiter=";")
+grouped_trips = trips.groupby(["type of destination", "type of trip"]).sum()
+grouped_trips_transposed = grouped_trips.T
+new_indices_latest = new_indices.append("Q3 2020")
+trips_grouped = grouped_trips_transposed.rename(index=dict(zip(grouped_trips_transposed.index,new_indices)))
+trips = trips.rename(columns=dict(zip(trips.columns[2:],new_indices)))
+domestic_trips = trips[trips["type of trip"] == "Total trips domestic"]
+
+
+# In[11]:
+
+
+alcohol_hovertemplate="%{y} litres<extra></extra>"
+alcohol_title_text = "Sales of alcoholic beverages per capita"
+
+def get_alcohol_sales_previous_year():
+    return {
+            'data': [dict(
+                x=alcohol_sales.index,
+                y=alcohol_sales["Q2 2019 Sales"],
+                type='bar',
+                marker={
+                    'color': 'orange'
+                },
+                name="Q2 2019",
+                hovertemplate=alcohol_hovertemplate,
+            ),
+            dict(
+                x=alcohol_sales.index,
+                y=alcohol_sales["Q2 2020 Sales"],
+                type='bar',
+                marker={
+                    'color': 'red'
+                },
+                name="Q2 2020",
+                hovertemplate=alcohol_hovertemplate,
+            )],
+            'layout': dict(
+                margin={'l': 40, 'b': 30, 't': 60, 'r': 0},
+                hovermode='closest',
+                title={
+                'text': getTitleText(alcohol_title_text),
+                'y':0.9,
+                'x':0.5,
+                'xanchor': 'center',
+                'yanchor': 'top',
+                'font': dict(size=20),
+                },
+                hoverlabel=dict(
+                    bgcolor="white",
+                    font=dict(
+                        size=16,
+                        family="Rockwell"
+                    ),
+                ),
+            )
+        }
+
+def get_alcohol_sales_trend():
+    data = []
     
-def getType(column):
-    if(column in numeric_columns):
-        return "numeric"
-    return "text"
+    for alcohol in alcohol_sales_all.columns:
+        sales = alcohol_sales_all[alcohol]
+        sub_data = dict(
+            x=sales.index,
+            y=sales,
+            type='scatter',
+            name=alcohol,
+            hovertemplate=alcohol +" : "+ alcohol_hovertemplate,
+        )
+        data.append(sub_data)
+        
+    return {
+            'data': data,
+            'layout': dict(
+            margin={'l': 40, 'b': 30, 't': 60, 'r': 0},
+            hovermode='x unified',
+            title={
+            'text': getTitleText(alcohol_title_text),
+            'y':0.9,
+            'x':0.5,
+            'xanchor': 'center',
+            'yanchor': 'top',
+            'font': dict(size=20),
+            },
+            hoverlabel=dict(
+                bgcolor="white",
+                font=dict(
+                    size=16,
+                    family="Rockwell"
+                ),
+            ),
+            legend=dict(
+                title=dict(
+                    text="Alcoholic beverages",
+                )
+            ),
+        )
+    }
 
 
-def getTitleText(text):
-    return text.title().replace('_', ' ').replace("Gdp", "GDP")
+# In[12]:
 
 
-# In[33]:
+trips_hovertemplate="%{y} million<extra></extra>"
+trips_title_text = "Domestic trips"
+
+def get_trips_previous_year():
+    return {
+        'data': [dict(
+            x=domestic_trips["type of destination"],
+            y=domestic_trips["Q3 2019"],
+            type='bar',
+            marker={
+                'color': 'lime'
+            },
+            hovertemplate=trips_hovertemplate,
+            name="Q3 2019",
+        ),
+        dict(
+            x=domestic_trips["type of destination"],
+            y=domestic_trips["Q3 2020"],
+            type='bar',
+            marker={
+                'color': 'green'
+            },
+            hovertemplate=trips_hovertemplate,
+            name="Q3 2020",
+        )],
+        'layout': dict(
+            margin={'l': 40, 'b': 30, 't': 60, 'r': 0},
+            hovermode='closest',
+            title={
+            'text': getTitleText(trips_title_text),
+            'y':0.9,
+            'x':0.5,
+            'xanchor': 'center',
+            'yanchor': 'top',
+            'font': dict(size=20),
+            },
+            hoverlabel=dict(
+                bgcolor="white",
+                font=dict(
+                    size=16,
+                    family="Rockwell"
+                ),
+            ),
+        )
+    }
+
+def get_trips_trend():
+    data = []
+    
+    for type_of_destination in trips_grouped.columns.get_level_values("type of destination").unique():
+        trips = trips_grouped[type_of_destination]["Total trips domestic"]
+        sub_data = dict(
+            x=trips_grouped.index,
+            y=trips,
+            type='scatter',
+            name=type_of_destination,
+            hovertemplate=type_of_destination +" : "+ trips_hovertemplate,
+        )
+        data.append(sub_data)
+        
+    return {
+            'data': data,
+            'layout': dict(
+            margin={'l': 40, 'b': 30, 't': 60, 'r': 0},
+            hovermode='x unified',
+            title={
+            'text': getTitleText(trips_title_text),
+            'y':0.9,
+            'x':0.5,
+            'xanchor': 'center',
+            'yanchor': 'top',
+            'font': dict(size=20),
+            },
+            hoverlabel=dict(
+                bgcolor="white",
+                font=dict(
+                    size=16,
+                    family="Rockwell"
+                ),
+            ),
+            legend=dict(
+                title=dict(
+                    text="Type of destination",
+                )
+            ),
+        )
+    }
+
+
+# In[13]:
 
 
 # Daily stats tab content
@@ -124,7 +360,57 @@ daily_stats_div = html.Div([
 ], className="container-fluid")
 
 
-# In[34]:
+# In[14]:
+
+
+# Datasets tab content
+datasets_options = ["previous_year", "show_trend"]
+datasets_div = html.Div([
+    html.Div(html.Div(html.H4("Impact of COVID-19 pandemic on Norwegian population"), className="col p-0"),
+             className="row mt-2"),
+    html.Div([
+        html.Div(
+            html.Div([
+                html.Div(    
+                    html.Div(
+                        dcc.RadioItems(
+                            id='alcohol-stats-type',
+                            options=[{'label': getTitleText(i), 'value': i} for i in datasets_options ],
+                            value=datasets_options[0],
+                            labelClassName="mr-2"
+                        ), className="col"), className="row mt-2"),
+
+                    html.Div(html.Div(dcc.Graph(id='alcohol-stats'), className="col"),
+                             className="row align-items-center"),
+
+                ], className="container-fluid"), className="col"),
+
+        html.Div(
+            html.Div([
+                html.Div(    
+                    html.Div(
+                        dcc.RadioItems(
+                            id='trips-stats-type',
+                            options=[{'label': getTitleText(i), 'value': i} for i in datasets_options ],
+                            value=datasets_options[0],
+                            labelClassName="mr-2"
+                        ), className="col"), className="row mt-2"),
+
+                    html.Div(html.Div(dcc.Graph(id='trips-stats'), className="col"), 
+                         className="row align-items-center"),
+
+                ], className="container-fluid"), className="col"),
+    
+    ], className="row"),
+    
+    html.Div(html.Div([
+        "Source: ", html.A("Statistics Norway", href="https://www.ssb.no/en/statbank", target="_blank")
+    ], className="col"), className="row"),
+    
+],className="container-fluid")
+
+
+# In[15]:
 
 
 world_table = dt.DataTable(
@@ -171,7 +457,7 @@ world_table = dt.DataTable(
 )
 
 
-# In[35]:
+# In[16]:
 
 
 # World data tab cotent
@@ -258,7 +544,7 @@ world_data_div = html.Div([
 ], className="container-fluid")
 
 
-# In[36]:
+# In[17]:
 
 
 # Comparison tab content
@@ -289,7 +575,7 @@ comparison_div = html.Div([
 ], className="container-fluid")
 
 
-# In[37]:
+# In[18]:
 
 
 # Analysis tab content
@@ -324,7 +610,7 @@ analysis_div = html.Div([
 ], className="container-fluid")
 
 
-# In[38]:
+# In[19]:
 
 
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
@@ -353,13 +639,14 @@ app.layout = html.Div([
         dcc.Tab(label='Analysis', children=[
             analysis_div
         ]),
-        dcc.Tab(label='Dataset', children=[
+        dcc.Tab(label='Impact Study', children=[
+            datasets_div
         ]),
     ]),className="col"),className="row"),
 ], className="container-fluid")
 
 
-# In[39]:
+# In[20]:
 
 
 def get_empty_graph(text):
@@ -420,7 +707,7 @@ def get_daily_graph(x, y, text):
     }
 
 
-# In[40]:
+# In[21]:
 
 
 # Daily stats tab callback
@@ -441,7 +728,31 @@ def update_daily_stats(location):
             get_daily_graph(dff.date, dff.new_tests,"Daily New Tests"))
 
 
-# In[41]:
+# In[22]:
+
+
+# Alcohol sales callback
+@app.callback(Output('alcohol-stats', 'figure'), Input('alcohol-stats-type', 'value'))
+def update_alcohol_sales_plot(alcohol_stats_type):
+    if (alcohol_stats_type == datasets_options[0]):
+        return get_alcohol_sales_previous_year()
+    else:
+        return get_alcohol_sales_trend()
+
+
+# In[23]:
+
+
+# Trips callback
+@app.callback(Output('trips-stats', 'figure'), Input('trips-stats-type', 'value'))
+def update_alcohol_sales_plot(alcohol_stats_type):
+    if (alcohol_stats_type == datasets_options[0]):
+        return get_trips_previous_year()
+    else:
+        return get_trips_trend()
+
+
+# In[24]:
 
 
 # Comparison tab callback
@@ -488,7 +799,7 @@ def update_comparison_plot(locations, comparison_type):
     }
 
 
-# In[42]:
+# In[25]:
 
 
 # Map callback
@@ -545,7 +856,7 @@ def display_map(map_type):
     }
 
 
-# In[43]:
+# In[26]:
 
 
 def get_click_data(clickData):
@@ -567,7 +878,7 @@ def get_click_data(clickData):
             dfcountry.total_deaths.apply(int_formatter), dfcountry.total_deaths_per_million.apply(float_formatter))
 
 
-# In[44]:
+# In[27]:
 
 
 # Map click callback
@@ -590,7 +901,7 @@ def display_click_data(clickData, clicks):
         return get_click_data(clickData)
 
 
-# In[45]:
+# In[28]:
 
 
 # World table callback
@@ -615,7 +926,7 @@ def update_table(table_type):
     return data, columns, sort_by
 
 
-# In[46]:
+# In[29]:
 
 
 # Analysis rank table callback
@@ -685,7 +996,26 @@ def update_analysis_graph(analysis_type, options):
     }
 
 
-# In[47]:
+# In[30]:
+
+
+# dfgm = pd.read_csv("Global_Mobility_Report.csv", dtype={"country_region_code": "string",
+#                                                  "country_region": "string",
+#                                                  "sub_region_1": "string",
+#                                                  "sub_region_2": "string",
+#                                                  "metro_area": "string",
+#                                                  "iso_3166_2_code": "string",
+#                                                  "census_fips_code": "string",
+#                                                  })
+
+
+# In[31]:
+
+
+# dfgm[dfgm.sub_region_2 == "Ålesund Municipality"]
+
+
+# In[32]:
 
 
 if __name__ == '__main__':
